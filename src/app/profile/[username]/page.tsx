d4109485a110337b585186be2bca4c3bb885cf90
Vashae <vashaeb2@gmail.com>
2025-02-10 14:37:16 -0500

    Full Social app complete with Dynamic routing to user profiles, instant feedback on likes, and much more!!!

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -1,11 +1,34 @@
+import { getProfileByUsername, isFollowing, getUserLikedPosts, getUserPosts} from "@/actions/profile.action"
+import { notFound } from "next/navigation";
+import ProfilePageClient from "./ProfilePageClient";
 
+export async function generateMetaData({params}: {params: {username:string}}){
+  const user = await getProfileByUsername(params.username)
+  if(!user) return;
 
-async function ProfilePage({params}: {params:{username:string}}) {
- console.log("params:", params)
+  return {
+    title: `${user.name ?? user.username}`,
+    description: user.bio || `Check out ${user.username}'s profile.`,
+  }
+}
+
+async function ProfilePageServer({params}: {params:{username:string}}) {
  await new Promise ((resolve) => setTimeout(resolve, 3000))
+ const user = await getProfileByUsername(params.username)
+ if (!user) notFound()
+ const [posts, likedPosts, isCurrentUserFollowing] = await Promise.all([
+  getUserPosts(user.id),
+  getUserLikedPosts(user.id),
+  isFollowing(user.id),
+ ])
     return (
-    <div> ProfilePage </div>
+   <ProfilePageClient
+   user={user}
+   posts={posts}
+   likedPosts={likedPosts}
+   isFollowing={isCurrentUserFollowing}
+   />
   )
 }
 
-export default ProfilePage
+export default ProfilePageServer
